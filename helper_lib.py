ff97397f4897242f6760a0eef42cc81d99613eb7
Cyrill Leutwiler <bigcyrill@hotmail.com>
2016-08-17 21:34:46 +0200

    Create helper_lib.py

---
+++ helper_lib.py
@@ -0,0 +1,36 @@
+import collections
+from nltk.corpus import stopwords
+from nltk.collocations import BigramCollocationFinder
+from nltk.metrics import BigramAssocMeasures
+
+def bag_of_words(words):
+    return dict([(word, True) for word in words])
+
+def bag_of_words_not_in_set(words, badwords):
+    return bag_of_words(set(words) - set(badwords))
+
+def bag_of_non_stopwords(words, stopfile='english'):
+    badwords = stopwords.words(stopfile)
+    return bag_of_words_not_in_set(words, badwords)
+
+def bag_of_bigrams_words(words, score_fn=BigramAssocMeasures.chi_sq, n=200):
+    bigram_finder = BigramCollocationFinder.from_words(words)
+    bigrams = bigram_finder.nbest(score_fn, n)
+    return bag_of_words(words + bigrams)
+
+def label_feats_from_corpus(corp, feature_detector=bag_of_words):
+    label_feats = collections.defaultdict(list)
+    for label in corp.categories():
+        for fileid in corp.fileids(categories=[label]):
+            feats = feature_detector(corp.words(fileids=[fileid]))
+            label_feats[label].append(feats)
+    return label_feats
+
+def split_label_feats(lfeats, split=0.75):
+    train_feats = []
+    test_feats = []
+    for label, feats in lfeats.items():
+        cutoff = int(len(feats) * split)
+        train_feats.extend([(feat, label) for feat in feats[:cutoff]])
+        test_feats.extend([(feat, label) for feat in feats[cutoff:]])
+    return train_feats, test_feats
