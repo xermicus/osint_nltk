3b4806de917fc1e9b098bfa63f9838b39c8f4c3f
Cyrill Leutwiler <bigcyrill@hotmail.com>
2016-08-22 05:50:23 +0000

    redis test

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import redis
 from nltk.corpus import movie_reviews
 from nltk.classify import NaiveBayesClassifier
 from helper_lib import label_feats_from_corpus, split_label_feats, token_of_words
@@ -18,9 +19,13 @@ def classify(sentence):
 	return classifier.classify(token_of_words(sentence))
 
 classifier = init_classifier()
+r = redis.StrictRedis(host='172.17.0.2', port=6379, password='letmeinplease', charset="utf-8", decode_responses=True)
 
 while True:
-	if classify(input("Tell me a sentence: ")) == 'pos':
+	text = input('Tell me a sentence: ')
+	r.set(text, classify(text))
+	result = r.get(text)
+	if result == 'pos':
 		print ('I think this is a positive sentence')
 	else:
 		print ('I think this is a negative sentence')
